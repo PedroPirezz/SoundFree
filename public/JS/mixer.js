7d6f2587d43556c0dcd393c5c61bf127622091f9
Pedro Henrieque Rodrigues Pires <foreverp333@gmail.com>
2026-01-07 01:14:59 -0300

    ajustes

--- public/JS/mixer.js
+++ public/JS/mixer.js
@@ -1,5 +1,6 @@
 const protocol = location.protocol === "https:" ? "wss" : "ws";
-const ws = new WebSocket(`${protocol}://${location.host}`);
+const ws = new WebSocket("https://soundfree-1.onrender.com/");
+
 
 const CHANNELS = 16;
 let mixerState = [];
